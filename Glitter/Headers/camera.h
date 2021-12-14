31e5e1c7dfc7be945b719689bd1608db09968a7a
Paxton Schweigert <paxmanrocks@gmail.com>
2021-12-14 15:14:21 -0800

    Add camera module with class Camera for user controlled camera input.

---
+++ Glitter/Headers/camera.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+enum class CameraMovement {
+    UPWARD,
+    DOWNWARD,
+    LEFT,
+    RIGHT
+};
+
+// a class stolen/inspired by https://learnopengl.com/code_viewer_gh.php?code=includes/learnopengl/camera.h
+class Camera {
+private:
+    glm::vec3 position;
+    const glm::vec3 front{0.0f, 0.0f, -1.0f};
+    const glm::vec3 up{0.0f, 1.0f, 0.0f};
+    const glm::vec3 right{1.0f, 0.0f, 0.0f};
+
+    // camera options
+    float movement_speed;
+    float mouse_sensitivity;
+    float zoom;
+public:
+    Camera(glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f), float speed_in = 2.5f, float sensitivity_in = 0.1f, float zoom_in = 45.0f)
+        : position{ pos }, movement_speed{ speed_in }, mouse_sensitivity{ sensitivity_in }, zoom{ zoom_in } {}
+    
+    // Return the view matrix based on the current camera location
+    glm::mat4 getViewMatrix() { return glm::lookAt(position, position + front, up); }
+    
+    // A callback for keyboard input to camera
+    void processKeyboard(CameraMovement direction, float delta_time);
+    // A callback for mouse input to camera
+    void processMouseMovement(float xoffset, float yoffset);
+    // A callback for mouse scrolling input. Simply subtracts the input from zoom.
+    void processMouseScroll(float scroll_length) { zoom -= scroll_length; }
+};
---
+++ Glitter/Sources/camera.cpp
@@ -0,0 +1,28 @@
+#include "camera.h"
+
+void Camera::processKeyboard(CameraMovement direction, float delta_time) {
+    float distance = movement_speed * delta_time;
+    switch (direction) {
+        case CameraMovement::UPWARD:
+            position += up * distance;
+            break;
+
+        case CameraMovement::DOWNWARD:
+            position -= up * distance;
+            break;
+
+        case CameraMovement::LEFT:
+            position -= right * distance;
+            break;
+
+        case CameraMovement::RIGHT:
+            position += right * distance;
+            break;
+    }
+}
+
+// TODO: Implement mouse movement for when cursor hits edge of screen like in
+// top down games.
+void Camera::processMouseMovement(float xoffset, float yoffset) {
+    
+}
