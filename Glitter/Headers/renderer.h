a335c94468bae289216f5f00e3b09a4bf983c758
Paxton Schweigert <paxmanrocks@gmail.com>
2021-12-13 22:46:36 -0800

    Added renderer module based off of learnopengl tutorial. This module includes a class CellRenderer to handle rendering of the cells in the GOL. It inits various OpenGL info to make life easier.

---
+++ Glitter/Headers/renderer.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "shader.h"
+
+#include "glm/glm.hpp"
+#include "glad/glad.h"
+#include "grid.h"
+
+#include <memory>
+#include <vector>
+
+// yoinked/inspired from https://learnopengl.com/In-Practice/2D-Game/Rendering-Sprites
+class CellRenderer {
+private:
+    std::shared_ptr<Shader> shader;
+    GLuint quadVAO{};
+
+    void initRenderData();
+public:
+    // Take a shader reference and make a shared_ptr from it
+    CellRenderer(std::shared_ptr<Shader> in_shader) : shader{in_shader} { initRenderData(); }
+
+    void DrawCell(const glm::vec2& pos, const glm::vec2& size = glm::vec2(10.0f, 10.0f),
+        const glm::vec3& color = glm::vec3(0.0f, 0.0f, 0.0f));
+};
+
+class GridRenderer {
+private:
+    using Coord = std::pair<int, int>;
+    using CoordVec = std::vector<Coord>;
+
+    Grid grid_bog;
+public:
+    GridRenderer(const CoordVec& in_vecs) : grid_bog{ in_vecs } {};
+    
+    void renderGrid(CellRenderer& renderer);
+};
---
+++ Glitter/Sources/renderer.cpp
@@ -0,0 +1,57 @@
+#include "renderer.h"
+#include "util.h"
+
+#include "glad/glad.h"
+#include "glm/ext/matrix_transform.hpp"
+
+// copy paste of learnopengl tutorial
+void CellRenderer::initRenderData() {
+    // configure VAO/VBO
+    unsigned int VBO;
+    float vertices[] = {
+        // pos      // tex
+        0.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 0.0f,
+
+        0.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &this->quadVAO);
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindVertexArray(this->quadVAO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void CellRenderer::DrawCell(const glm::vec2 &pos, const glm::vec2 &size, const glm::vec3 &color) {
+    // prepare transformations
+    shader->use();
+    glm::mat4 model{ 1.0f };
+    model = glm::translate(model, glm::vec3(pos,0));
+    
+    model = glm::scale(model, glm::vec3(size,1));
+
+    shader->setMatrix4("model", model);
+    shader->setVector3f("cell_color", color);
+
+    glBindVertexArray(this->quadVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+}
+
+
+void GridRenderer::renderGrid(CellRenderer& renderer) {
+    for (const Coord& coord : grid_bog.alive_cells) {
+        renderer.DrawCell(coordToVec(coord)*glm::vec2(20.0,20.0), {20.0f, 20.0f}, {1.0f, 1.0f, 1.0f});
+    }
+    grid_bog.update();
+}
