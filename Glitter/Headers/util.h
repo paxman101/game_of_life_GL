ec192b8b4cf966e6dc821bee2380b81accde80d6
Paxton Schweigert <paxmanrocks@gmail.com>
2021-12-15 15:17:18 -0800

    Added left shift overload for printing out GLM vectors

--- Glitter/Headers/util.h
+++ Glitter/Headers/util.h
@@ -9,7 +9,17 @@ inline glm::vec2 coordToVec(const std::pair<int, int>& in_pair) {
     return glm::vec2{ in_pair.first, in_pair.second };
 }
 
+
 namespace glm {
+    template<int size, typename T, qualifier Q>
+    std::ostream& operator<<(std::ostream& out, glm::vec<size, T, Q> const& v) {
+        for (int i = 0; i < size; i++) {
+            if (i != 0) out << " ";
+            out << v[i];
+        }
+        return out;
+    }
+
     // Add a translate for 2x2 vector
     template<typename T, qualifier Q>
     GLM_FUNC_QUALIFIER mat<2, 2, T, Q> translate(mat<2, 2, T, Q> const& m, vec<2, T, Q> const& v) {
